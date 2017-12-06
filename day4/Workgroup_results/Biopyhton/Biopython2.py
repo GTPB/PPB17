fea8ca1ea1ceced3fc3d841a24750cf65de4dd2b
Joana Marques <jamarques@igc.gulbenkian.pt>
2017-12-06 11:51:32 +0000

    Add files via upload

---
+++ day4/Workgroup_results/Biopyhton/Biopython2.py
@@ -0,0 +1,17 @@
+from Bio import Entrez
+from Bio import Medline
+keyword='Bacillus licheniformis'
+Entrez.email=''
+handle=Entrez.esearch(db='pubmed', term=keyword)
+record=Entrez.read(handle)
+pmids=record['IdList']
+print pmids
+handle=Entrez.efetch(db='pubmed', id=pmids, \
+    rettype='medline', retmode='text')
+medline_records=Medline.parse(handle)
+records=list(medline_records)
+n=1
+for record in records:
+    if keyword in record['TI']:
+        print n, ')', record['TI']
+        n += 1
